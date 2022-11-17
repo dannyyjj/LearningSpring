50dd50be62ed8b787a553f1d8a867934f1800ccb
정대현 <dannyyjj123@gmail.com>
2022-11-17 09:35:31 +0900

    h2.bat 켜져있어야 함

--- src/main/java/com/danny/learningspring/LearningSpringApplication.java
+++ src/main/java/com/danny/learningspring/LearningSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningSpringApplication {
-
+    // h2.bat 켜져있는지 확인 !!
     public static void main(String[] args) {
         SpringApplication.run(LearningSpringApplication.class, args);
     }
